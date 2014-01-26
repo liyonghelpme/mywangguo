31aaad985d7d72aca308dade14e33bf51cbb06c9
liyonghelpme <liyonghelpme@gmail.com>
2014-01-26 09:02:18 +0800

    SessionMenu

--- Resources/LuaScript/menu/SessionMenu.lua
+++ Resources/LuaScript/menu/SessionMenu.lua
@@ -11,16 +11,16 @@ function SessionMenu:ctor(word, callback, del)
     local but = ui.newButton({image="newClose.png", text="", font="f1", size=18, delegate=self, callback=closeDialog, shadowColor={0, 0, 0}, color={255, 255, 255}})
     but:setContentSize(80, 82)
     setPos(addChild(self.temp, but.bg), {813, fixY(sz.height, 196)})
-    local w = setPos(setAnchor(addChild(self.temp, ui.newTTFLabel({text=word, align=ui.TEXT_ALIGN_LEFT, valign=ui.TEXT_VALIGN_TOP, size=28, color={255, 255, 255}, font="f1", shadowColor={0, 0, 0}})), {0.00, 1.00}), {255, fixY(sz.height, 325)})
+    local w = setPos(setAnchor(addChild(self.temp, ui.newTTFLabel({text=word, dimensions=CCSizeMake(1024, 768), align=ui.TEXT_ALIGN_LEFT, valign=ui.TEXT_VALIGN_TOP, size=28, color={255, 255, 255}, font="f1", shadowColor={0, 0, 0}})), {0.00, 1.00}), {255, fixY(sz.height, 325)})
     local sp = setOpacity(setAnchor(setSize(setPos(addSprite(self.temp, "dialogCat.png"), {740, fixY(sz.height, 500)}), {101, 157}), {0.50, 0.50}), 255)
     local w = setPos(setAnchor(addChild(self.temp, ui.newTTFLabel({text="忍者喵", size=26, color={32, 112, 220}, font="f2", shadowColor={255, 255, 255}})), {0.00, 0.50}), {603, fixY(sz.height, 546)})
     local sp = setOpacity(setAnchor(setSize(setPos(addSprite(self.temp, "sessionTitle.png"), {523, fixY(sz.height, 233)}), {194, 38}), {0.50, 0.50}), 255)
     centerUI(self)
+    registerEnterOrExit(self)
 end
-
---关闭对话框的回调函数
-function SessionMenu:refreshData()
+function SessionMenu:exitScene()
     if self.callback ~= nil then
         self.callback(self.del)
     end
 end
+
